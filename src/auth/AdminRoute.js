c9a313b1e7172cfcac05e64f2b802289dbd5c6fe
Ahmed Abdelrazak <ahmedabdelrazzak1001010@gmail.com>
2025-12-21 10:54:57 -0800

    Added

--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -1,15 +1,29 @@
 /** @format */
 
 import React from "react";
+import { ConfigProvider } from "antd";
 import { Route, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./index";
+
+const resolvePopupContainer = (triggerNode) => {
+	if (typeof document === "undefined") {
+		return triggerNode || null;
+	}
+	if (!triggerNode) {
+		return document.body;
+	}
+	const container = triggerNode.closest(".ant-modal, .ant-drawer");
+	return container || document.body;
+};
 
 const AdminRoute = ({ component: Component, ...rest }) => (
 	<Route
 		{...rest}
 		render={(props) =>
 			isAuthenticated() && isAuthenticated().user.role === 1000 ? (
-				<Component {...props} />
+				<ConfigProvider getPopupContainer={resolvePopupContainer}>
+					<Component {...props} />
+				</ConfigProvider>
 			) : (
 				<Redirect
 					to={{
